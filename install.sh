a8f057bb996e4d4daeee72d27a8db0075afffaeb
Jeevitha Kanann K S <ksjeevithakannan123@gmail.com>
2024-06-18 11:55:55 +0530

    Initial commit

---
+++ install.sh
@@ -0,0 +1,49 @@
+DOT_LOCATION=~/dot-jeeva
+
+gitclone() {
+	echo "Clone dot files into ~/dot-jeeva"
+	if [ -d "$DOT_LOCATION" ]; then
+		read -n 1 -p "Remove all contents in $DOT_LOCATION [Y/N] [DEFAULT Y]: " confirm
+		echo
+		confirm=${confirm:-Y}
+		if [[ "$confirm" =~ [yY] ]]; then
+			echo "Cleaning $DOT_LOCATION"
+			cd "$DOT_LOCATION"
+			rm -rf *
+		else
+			exit 1
+		fi
+	else
+		mkdir "$DOT_LOCATION"
+	fi
+	git clone https://github.com/jeevithakannan2/my-dwm.git --depth 1 "$DOT_LOCATION/my-dwm"
+	git clone https://github.com/jeevithakannan2/my-dwmstatus.git --depth 1 "$DOT_LOCATION/my-dwmstatus"
+}
+
+install() {
+	cd "$DOT_LOCATION"
+	sudo make install ./my-dwm/
+	sudo make install ./my-dwmstatus/
+}
+
+if command -v pacman &>/dev/null; then
+	echo "Arch System !!!"
+	if command -v git &>/dev/null; then
+		echo "Git found in system"
+		gitclone
+		if [ $? -ne 0 ]; then
+			echo "Cloning failed !!"
+		fi
+	else
+		echo "Git not found in system installing git"
+		sudo pacman -Syu git
+		gitclone
+		if [ $? -ne 0 ]; then
+			echo "Cloning failed !!"
+		fi
+	fi
+
+else
+	echo "Arch system not found"
+	exit 1
+fi
